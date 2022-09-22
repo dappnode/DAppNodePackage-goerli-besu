28ae40d7829b374ac9fe83b2c0efec2ae6d687a8
Voss <alex@visnovaventures.com>
2022-09-22 01:21:50 -0500

    StakersUI debug

--- Dockerfile
+++ Dockerfile
@@ -2,32 +2,15 @@ ARG UPSTREAM_VERSION
 
 FROM hyperledger/besu:${UPSTREAM_VERSION}
 
-COPY jwtsecret.hex /jwtsecret
+COPY /security /security
+COPY entrypoint.sh /usr/local/bin/entrypoint.sh
 
 USER root
-RUN mkdir /var/lib/besu-goerli
-RUN chown -R besu:besu /var/lib/besu-goerli
+RUN apt update && apt install curl -y
+
+
 USER besu
 
 ENV BESU_OPTS=$BESU_OPTS
 
-ENTRYPOINT besu \
-    --rpc-ws-host='0.0.0.0' \
-    --network=goerli \
-    --rpc-ws-enabled=$WS_ENABLED \
-    --rpc-http-host='0.0.0.0' \
-    --rpc-http-enabled=true \ 
-    --host-allowlist=* \
-    --rpc-http-cors-origins=* \
-    --engine-rpc-port=8551 \
-    --engine-host-allowlist=* \
-    --engine-jwt-secret='/jwtsecret' \
-    --engine-rpc-enabled=true \
-    --data-storage-format=$STORAGE_FORMAT \
-    --metrics-enabled \
-    --metrics-host='0.0.0.0' \
-    --data-path=/var/lib/besu-goerli \
-    --sync-mode=$SYNC_MODE \
-    --rpc-http-max-active-connections=$MAX_HTTP_CONNECTIONS \
-    --p2p-port=$P2P_PORT \
-    $EXTRA_OPTS
+ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_PRATER" in
+"prysm-prater.dnp.dappnode.eth")
+  echo "Using prysm-prater.dnp.dappnode.eth"
+  JWT_PATH="/security/prysm/jwtsecret.hex"
+  ;;
+"lighthouse-prater.dnp.dappnode.eth")
+  echo "Using lighthouse-prater.dnp.dappnode.eth"
+  JWT_PATH="/security/lighthouse/jwtsecret.hex"
+  ;;
+"teku-prater.dnp.dappnode.eth")
+  echo "Using teku-prater.dnp.dappnode.eth"
+  JWT_PATH="/security/teku/jwtsecret.hex"
+  ;;
+"nimbus-prater.dnp.dappnode.eth")
+  echo "Using nimbus-prater.dnp.dappnode.eth"
+  JWT_PATH="/security/nimbus/jwtsecret.hex"
+  ;;
+*)
+  echo "Using default"
+  JWT_PATH="/security/default/jwtsecret.hex"
+  ;;
+esac
+
+# Print the jwt to the dappmanager
+JWT=$(cat $JWT_PATH)
+curl -X POST "http://my.dappnode/data-send?key=jwt&data=${JWT}"
+
+exec besu \
+    --rpc-ws-host='0.0.0.0' \
+    --network=goerli \
+    --rpc-ws-enabled=$WS_ENABLED \
+    --rpc-http-host='0.0.0.0' \
+    --rpc-http-enabled=true \ 
+    --host-allowlist=* \
+    --rpc-http-cors-origins=* \
+    --engine-rpc-port=8551 \
+    --engine-host-allowlist=* \
+    --engine-jwt-secret='/jwtsecret' \
+    --engine-rpc-enabled=true \
+    --data-storage-format=$STORAGE_FORMAT \
+    --metrics-enabled \
+    --metrics-host='0.0.0.0' \
+    --data-path=/besu-goerli \
+    --sync-mode=$SYNC_MODE \
+    --rpc-http-max-active-connections=$MAX_HTTP_CONNECTIONS \
+    --p2p-port=$P2P_PORT \
+    $EXTRA_OPTS
